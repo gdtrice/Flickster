11babce41a0b4b251ae505fca6e6377d545ca7a9
Garland Trice <GDTrice@hotmail.com>
2016-08-07 21:19:37 -0700

    implement view holder pattern

--- app/src/main/java/com/example/gtrice/flickster/adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/example/gtrice/flickster/adapters/MovieArrayAdapter.java
@@ -18,6 +18,7 @@ import java.util.List;
 
 import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
 
+
 /**
  * Created by gtrice on 8/1/16.
  */
@@ -30,23 +31,26 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
     public View getView(int position, View convertView, ViewGroup parent) {
         Movie movie = getItem(position);
 
+        MovieArrayAdapterViewHolder viewHolder;
         // check the view is being reused
         if (convertView == null) {
+            viewHolder = new MovieArrayAdapterViewHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.item_movie, parent, false);
+            viewHolder.ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+            viewHolder.tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+            viewHolder.tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+            convertView.setTag(viewHolder);
+
+        } else {
+            viewHolder = (MovieArrayAdapterViewHolder) convertView.getTag();
         }
 
-        // find the image view
-        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
-
         // clear out image from convertView
-        ivImage.setImageResource(0);
+        viewHolder.ivImage.setImageResource(0);
+        viewHolder.tvTitle.setText(movie.getOriginalTitle());
+        viewHolder.tvOverview.setText(movie.getOverview());
 
-        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
-
-        tvTitle.setText(movie.getOriginalTitle());
-        tvOverview.setText(movie.getOverview());
         Log.d("poster %s", movie.getPosterPath());
 
         int orientation = getContext().getResources().getConfiguration().orientation;
@@ -54,11 +58,11 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
         if(orientation == Configuration.ORIENTATION_LANDSCAPE) {
             Picasso.with(getContext()).load(movie.getBackdropPath())
                     .transform(new RoundedCornersTransformation(20, 20))
-                    .into(ivImage);
+                    .into(viewHolder.ivImage);
         } else if (orientation == Configuration.ORIENTATION_PORTRAIT) {
             Picasso.with(getContext()).load(movie.getPosterPath())
                     .transform(new RoundedCornersTransformation(20, 20))
-                    .into(ivImage);
+                    .into(viewHolder.ivImage);
         }
 
         return convertView;
---
+++ app/src/main/java/com/example/gtrice/flickster/adapters/MovieArrayAdapterViewHolder.java
@@ -0,0 +1,14 @@
+package com.example.gtrice.flickster.adapters;
+
+/**
+ * Created by gtrice on 8/7/16.
+ */
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MovieArrayAdapterViewHolder {
+    ImageView ivImage;
+    TextView tvTitle;
+    TextView tvOverview;
+}
